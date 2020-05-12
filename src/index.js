52f1f2c6803573fa4451cb1fe9e8bc27565c576b
pratikrana1998 <pratikrana1998@users.noreply.github.com>
2020-05-12 23:58:49 +0530

    fixing minor deploy issues

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
-import './index.css';
+import { createLogger } from 'redux-logger';
+import 'tachyons';
+
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-import { searchRobots, requestRobots } from './reducers';
-import 'tachyons';
- 
-const logger = createLogger();
+import { requestRobots, searchRobots } from './reducers'
 
-const rootReducer = combineReducers({searchRobots, requestRobots})
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
+import './index.css';
+
+const logger = createLogger()
+
+const rootReducers = combineReducers({requestRobots, searchRobots})
+
+const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger))
 
 ReactDOM.render(
-                <Provider store={store}>
-                <App />
-                </Provider>, document.getElementById('root'));
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
